daacb58003e892474a2d7a1112f434e74b864e73
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-06 14:02:42 +0200

    Impostazione iniziale

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*Scrivi un programma che stampi in console i numeri da 1 a 100, 
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e 
+per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 
+che di 5 stampi “FizzBuzz” */
+
+/*BONUS 1:
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) 
+un elemento html con il numero o la stringa corretta da mostrare.
+BONUS 2:
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, 
+a seconda che il valore inserito sia un numero, 
+un fizz, un buzz o un fizzbuzz.*/
+
+/////////////////////////////////////////////////////////////////////////
+
+//creare un'istruzione che consenta di generare numeri da 1 a 100 ciclicamente
+
+// creare condizioni all'interno di un ciclo
+    // se i divisibile per 3 = sostituire Fizz al numero
+    // se i è divisibile per 5 = sostituire Buzz al numero
+    // se i è divisibile per 5 e per 3 = sostituire FizzBuzz al numero;
+    // altrimenti genera il numero
+//stampare in log
